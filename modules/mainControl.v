c701cb65c4ac5c2cf7ef501bf21f40d49412f545
Baconhead78 <gonzalo.alfacaso@gmail.com>
2019-06-17 02:56:31 -0500

    Added R-format, lw and sw to the MainControl

--- modules/mainControl.v
+++ modules/mainControl.v
@@ -1,4 +1,37 @@
-module mainControl(opcode,Jump,Branch,RegDst,RegWrite,ALUSrc,PCSrc,MemRead,MemWrite,MemtoReg,ALUOp);
+module mainControl(opcode,RegDst,Jump,Branch,MemRead,MemWrite,MemtoReg,ALUOp,ALUSrc,RegWrite);
 input [5:0] opcode;
-output Branch,MemRead,MemWrite,ALUSrc,RegWrite;
-output [1:0] RegDst,MemtoReg,ALUOp,Jump;
+output reg Branch,MemRead,MemWrite,ALUSrc,RegWrite;
+output reg [1:0] RegDst,MemtoReg,ALUOp,Jump;
+
+always @(*)
+begin
+	case (opcode)
+		6'b0: // R-type - 0
+		begin
+		 RegDst = 1;
+		 ALUSrc = 0;
+		 MemtoReg = 0;
+		 RegWrite = 1;
+		 MemRead = 0;
+		 MemWrite = 0;
+		 Branch = 0;
+		 ALUOp = 2'b10;
+		end
+		
+		6'b100011: // Load word - 35
+		begin
+			RegDst = 0;
+			ALUSrc = 1;
+			MemtoReg = 1;
+			RegWrite = 1;
+			MemRead = 1;
+			MemWrite = 0;
+			Branch = 0;
+			ALUOp = 2'b00;
+		end
+		
+		
+	endcase
+end
+
+endmodule
